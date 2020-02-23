64bc67e8a55c5644dbc108c8edc96238eb2e7441
Tyler Senne <tsenne2@huskers.unl.edu>
2020-02-23 11:49:58 -0600

    Created basic character and camera controller scripts

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float minY = -90f;
+    public float maxY = 90f;
+    public float sensitivityX = 15f;
+    public float sensitivityY = 15f;
+
+    public Camera cam;
+
+    private float rotationY = 0f;
+    private float rotationX = 0f;
+
+    void Start() 
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update() 
+    {
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+        rotationY = Mathf.Clamp(rotationY, minY, maxY);
+
+        transform.localEulerAngles = new Vector3(0, rotationX, 0);
+        cam.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody rb;
+    
+    private Vector2 input;
+    private Vector3 moveDirection;
+
+    public float walkSpeed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        moveDirection = (moveHorizontal * transform.right + moveVertical * transform.forward).normalized;
+        Debug.Log(moveDirection);
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+    }
+
+    void FixedUpdate() 
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 yVelocity = new Vector3(0, rb.velocity.y, 0);
+        rb.velocity = moveDirection * walkSpeed * Time.deltaTime;
+        rb.velocity += yVelocity;
+    }
+}
